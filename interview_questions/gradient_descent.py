81defd0f7f54df06f9a66d83ae7ae28795475137
Eric Ma <ericmajinglong@gmail.com>
2017-07-16 19:35:47 -0400

    Updated gradient descent with correct solution

--- interview_questions/gradient_descent.py
+++ interview_questions/gradient_descent.py
@@ -1,25 +1,37 @@
+"""
+Question: Define gradient descent.
+
+For the case of y = x^2 + 3x + 1, implement gradient descent in Python to find
+the minima.
+
+The function should take in
+- a starting point `x`
+- a precision parameter `k` that specifies how much error is allowed.
+- a learning rate parameter `lr` that controls the rate of descent.
+"""
+
+
 def f(x):
-    return x ** 2 - 2 * x
+    return x ** 2 + 3 * x + 1
 
 
 def df(x):
-    return 2 * x - 2
+    return 2 * x + 3
 
 
 def gd(x, k, lr):
-    y = f(x)
+    prev_step_size = x
 
-    for _ in range(k):
-        x_prop = x - df(x) * lr  # propose a new x to move to
-        if f(x_prop) > y:
-            break
-        else:
-            x = x_prop
-            y = f(x)
-            lr = lr * 0.999999999
+    while prev_step_size > k:
+        # print(x)
+        prev_x = x
+        x += - df(prev_x) * lr
+        prev_step_size = abs(x - prev_x)
+
+    y = f(x)
 
     return (x, y)
 
 
-x, y = gd(10, 20000, 0.001)
+x, y = gd(15, 0.000001, 0.01)
 print(round(x, 3), round(y, 3))
